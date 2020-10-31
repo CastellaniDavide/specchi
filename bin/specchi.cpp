c8bbba81682730ca3288ba13bf0d6d051f8b4160
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-10-31 17:22:22 +0100

    Created bin/template.cpp

---
+++ bin/specchi.cpp
@@ -0,0 +1,42 @@
+/**
+ * @file specchi.cpp
+ *
+ * @version 01.01 2020-10-31
+ *
+ * @brief https://training.olinfo.it/#/task/oii_specchi/
+ *
+ * @ingroup specchi
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+
+// Includes
+#include <bits/stdc++.h>
+using namespace std;
+
+// Variabiles
+int N;
+
+// Main code
+int main()
+{
+  // Cncomment the following lines if you want to read/write from files
+  // freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+
+  // Input
+  cin >> N;
+
+  // Code
+  // ...
+
+  // Output
+  cout << N << endl;
+
+  // End
+  return 0;
+}
